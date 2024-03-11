f1a2bb737e40cb14ed10770c1f997af0a67dff78
Sushant Kumar <sushantkumar23@gmail.com>
2024-03-11 18:11:25 +0530

    Added resumer_parser.py and resume_tools

---
+++ resume_parser.py
@@ -0,0 +1,36 @@
+# resume_parser.py
+import json
+
+from models import Resume
+from tools import resume_tools
+
+
+class ResumeParser:
+    """
+    ResumeParser is a class that is used to parse the resume using the LLM Function call
+
+    Methods:
+    - parse: This method takes the resume text as input and returns a Resume object
+    """
+
+    def __init__(self, llm_client):
+        self.llm_client = llm_client
+
+    def parse(self, text):
+        response = self.llm_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Parse the resume that has been provided"
+                },
+                {
+                    "role": "user",
+                    "content": text
+                }
+            ],
+            tools=resume_tools
+        )
+        parsed_dict =  json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+        resume = Resume(**parsed_dict)
+        return resume
--- tools.py
+++ tools.py
@@ -42,3 +42,59 @@ job_description_tools = [
 
     }
 ]
+
+resume_tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_resume_data",
+            "description": "Parse the resume",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "candidate_name": {
+                        "type": "string",
+                        "description": "The candidate's name"
+                    },
+                    "email": {
+                        "type": "string",
+                        "description": "The candidate's email"
+                    },
+                    "phone": {
+                        "type": "string",
+                        "description": "The candidate's phone number"
+                    },
+                    "location": {
+                        "type": "string",
+                        "description": "The location"
+                    },
+                    "industry": {
+                        "type": "array",
+                        "items": {
+                            "type": "string",
+                            "description": "The industry/domain"
+                        },
+                        "description": "The industries/domains"
+                    },
+                    "education_degree": {
+                        "type": "array",
+                        "items": {
+                            "type": "string",
+                            "description": "The education degree"
+                        },
+                        "description": "The education degrees"
+                    },
+                    "skills": {
+                        "type": "array",
+                        "items": {
+                            "type": "string",
+                            "description": "The skills"
+                        },
+                        "description": "The skills"
+                    }
+                },
+                "required": ["candidate_name", "location", "industry", "education_degree", "skills"]
+            }
+        }
+    }
+]
